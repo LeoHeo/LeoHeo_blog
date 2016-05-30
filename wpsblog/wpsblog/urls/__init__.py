1e7de01e86950acf4321f20e2df18eb270ce9a2c
LeoHeo <hjh5488@gmail.com>
2016-05-30 15:40:20 +0800

    refactor urls.py separate urlpatterns

---
+++ wpsblog/wpsblog/urls/__init__.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from wpsblog.views import *
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+
+    url(r'^$', home, name="home"),
+    url(r'^rooms/(?P<room_id>\d+)/$', room, name="room"),
+    url(r'^news/$', news, name="news"),
+    url(r'^about/us/$', about, name="about"),
+
+    url(r'^policy/',include("wpsblog.urls.policy", namespace="policy"))
+]
+
+# 1. policy_urlpatterns 변수로
+# 2. policy_urlpatterns를 파일로 (from .. import ..)
+# 3. policy_urls라는 모듈로 (include 가 모듈이름을 받을 수 있었다.)
