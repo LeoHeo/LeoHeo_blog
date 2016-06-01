b3837606d2e28f90b904d546f40b72fd695c718c
LeoHeo <hjh5488@gmail.com>
2016-06-01 15:18:49 +0800

    refactor naver_blog_crawler: keyword, query search django filter usage

--- wpsblog/wpsblog/views/naver_blog_crawler.py
+++ wpsblog/wpsblog/views/naver_blog_crawler.py
@@ -5,21 +5,22 @@ from wpsblog.models import NaverPost
 
 def naver_blog_crawler(request):
     keyword = request.GET.get('keyword')
+    query = request.GET.get('query')
+
     post_list = NaverPost.objects.all()
 
     if keyword:
-        post_list = [
-            post
-            for post
-            in post_list
-            if post.keyword == keyword
-        ]
+        post_list = post_list.filter(keyword=keyword)
+
+    if query:
+        post_list = post_list.filter(title__contains=query)
 
     return render(
         request,
         "naver_post/list.html",
         {
             "keyword": keyword,
+            "query": query,
             "post_list": post_list
         }
     )
