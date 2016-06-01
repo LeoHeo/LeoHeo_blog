c729fbde640e1a1f7811cf71e26b7cf84bbed308
LeoHeo <hjh5488@gmail.com>
2016-06-01 15:07:20 +0800

    change naver_blog_crawler:add keyword search feature

--- wpsblog/wpsblog/views/naver_blog_crawler.py
+++ wpsblog/wpsblog/views/naver_blog_crawler.py
@@ -4,8 +4,22 @@ from wpsblog.models import NaverPost
 
 
 def naver_blog_crawler(request):
+    keyword = request.GET.get('keyword')
+    post_list = NaverPost.objects.all()
+
+    if keyword:
+        post_list = [
+            post
+            for post
+            in post_list
+            if post.keyword == keyword
+        ]
+
     return render(
         request,
         "naver_post/list.html",
-        {"blog_list": NaverPost.objects.all()}
+        {
+            "keyword": keyword,
+            "post_list": post_list
+        }
     )
