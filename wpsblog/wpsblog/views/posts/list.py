2c54e4d93d3b0a0352cc82f60edc37d7e22dffa3
LeoHeo <hjh5488@gmail.com>
2016-06-12 05:50:14 +0800

    homework #3: add post list pagination

--- wpsblog/wpsblog/views/posts/list.py
+++ wpsblog/wpsblog/views/posts/list.py
@@ -1,12 +1,23 @@
+import math
+
 from django.shortcuts import render
 from wpsblog.models import Post
 
 
 def list(request):
+    page = int(request.GET.get('page') or 1)
+    per = int(request.GET.get('per') or 5)
+    pagination = math.ceil(Post.objects.public().count() / per)
+
+    start = (page * per) - per
+    end = (page * per)
+
     return render(
         request,
         "posts/list.html",
         {
-            "posts_list": Post.objects.public(),
+            "posts_list": Post.objects.public()[start:end],
+            "page_list": range(1, pagination+1),
+            "per": per,
         }
     )
