357eb2f72745e711d78996bf1f0008c4cda5681f
LeoHeo <hjh5488@gmail.com>
2016-06-13 13:59:14 +0800

    add model ForeignKey User

--- wpsblog/wpsblog/views/posts/comments/comment_create.py
+++ wpsblog/wpsblog/views/posts/comments/comment_create.py
@@ -7,6 +7,7 @@ def comment_create(request, post_id):
     post = Post.objects.get(id=post_id)
 
     comment = post.comment_set.create(
+        user = request.user,
         content=request.POST.get('content'),
     )
 
