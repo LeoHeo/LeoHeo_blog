35097f50160365570fdc8d1630d994865cfbef10
LeoHeo <hjh5488@gmail.com>
2016-06-15 01:28:27 +0800

    add admin register: BitLink, BitLinkLog

--- wpsblog/wpsblog/admin.py
+++ wpsblog/wpsblog/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 from wpsblog.models import Post, Comment, NaverPost
-
+from bitly.models import BitLink, BitLinkLog
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(NaverPost)
+admin.site.register(BitLink)
+admin.site.register(BitLinkLog)
