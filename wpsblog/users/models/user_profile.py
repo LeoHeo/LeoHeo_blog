c1f811503df639d971afc27d06cea852e6fbaabf
LeoHeo <hjh5488@gmail.com>
2016-06-15 16:51:02 +0800

    init UserProfile Model

--- wpsblog/users/models/user_profile.py
+++ wpsblog/users/models/user_profile.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class UserProfile(models.Model):
-    
+
     user = models.OneToOneField(User)
-    
+
     phonenumber = models.CharField(
         max_length=16,
         blank=True,
